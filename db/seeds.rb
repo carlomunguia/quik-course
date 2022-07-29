72c26558cd3c34a2138ff83ca5b85da6c5001ac5
Carlo Munguia <cmunguia@denimgroup.com>
2022-07-28 22:37:54 -0500

    Quik Course 5
    brought in faker, devise & action text and ran various migrations to update data tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  Course.create!(
+    [
+      {
+        title: Faker::Educator.course_name,
+        description: Faker::TvShows::TwinPeaks.quote
+      }
+    ]
+  )
+end
