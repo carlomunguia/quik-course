69cbf6e347dd199a40a1e8bd4398f2f81b8a41a2
Carlo Munguia <cmunguia@denimgroup.com>
2022-08-10 22:34:40 -0500

    Quik Course 18
    created editing roles functionality for the user, updated controller & created new edit view, updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :courses
-  resources :users, only: [:index]
+  resources :users, only: %i[index edit show update]
   root "static_pages#landing_page"
   get "static_pages/activity"
   # get "static_pages/privacy_policy"
